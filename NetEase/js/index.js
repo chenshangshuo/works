d22ef175817eff9af431dd3477af317e01b0cf6e
chenshangshuo <390532099@qq.com>
2017-09-05 16:56:16 +0800

    完成

--- NetEase/js/index.js
+++ NetEase/js/index.js
@@ -11,6 +11,91 @@
 	})
 	//移除登录图
 	$loading.remove()
+	//tab切换
+	$('.siteNav').on('click','li',function(e){
+		$(e.currentTarget).addClass('active').siblings().removeClass('active')
+		let index = $(e.currentTarget).index()
+		//click时，触发tabChange事件，并传递参数index
+		$(e.currentTarget).trigger('tabChange', index)
+		$('.content>li').eq(index).addClass('active').siblings().removeClass('active')
+	})
+	$('.siteNav').on('tabChange',function(e,index){
+		let $li = $('.content>li').eq(index)
+		//设置请求一次json
+		if ($li.attr('data-downloaded') === 'yes') {
+			return
+		}
+		if (index === 1) {
+			$.get('../tab1.json').then(function(responese){
+				let hotSongArr = responese
+				hotSongArr.forEach(function(song){
+					let $hotSong = $(`<li><a href="./song.html?id=${song.id}"><span>${song.order}</span><h3>${song.name}</h3>
+						<svg class="latestStart" aria-hidden="true"><use xlink:href="#icon-icon2"></use></svg>
+						<p><svg class="latestSq" aria-hidden="true"><use xlink:href="#icon-SQ"></use></svg>${song.singer}</p></a></li>`)
+					$('.hotSongLists').append($hotSong)
+				})
+				$li.attr('data-downloaded','yes')
+							console.log(1)
+			})
+		}else if (index === 2) {
+			$.get('../tab1.json').then(function(res){
+				let searchArr = res
+				searchArr.forEach(function(song){
+					let $hotSong = $(`<li><a href="./song.html?id=${song.id}"><span>${song.name}</span></li>`)
+					$('.hotLists').append($hotSong)
+				})
+				$li.attr('data-downloaded', 'yes')
+			})
+		}
+	})
+
+	//函数节流，利用定时器，减少dom操作次数
+	let timer = undefined
+	$('#search').on('input',function(e){
+		let $input = $(e.currentTarget)
+		let value = $input.val().trim()
+		if (value === '') {return}
+		if (timer) {
+			clearTimeout(timer)
+		}
+
+		timer = setTimeout(function(){
+			search(value).then(function(result){
+				timer = undefined
+				if (result.length !== 0) {
+					let name = result.map((r) => r.name)
+					let id = result.map((r) => r.id)
+					$('.opt').html(`<p><a href="./song.html?id=${id}">${name}</a></p>`)
+				}else{
+					$('.opt').text('没有结果')
+				}
+			})
+		},500)
+	})
+	//搜索关键字并匹配
+	function search(keyword){
+		return new Promise((resolve, reject) =>{
+			var database = [
+				{"id":0, "name":"海阔天空"},
+				{"id":1, "name":"磁带版A面末的口白"},
+				{"id":2, "name":"你好"},
+				{"id":3, "name":"神"},
+				{"id":4, "name":"歌谣"},
+				{"id":5, "name":"上火星球"}
+			]
+			let result = database.filter(function(item){
+				return item.name.indexOf(keyword) >=0
+			})
+			setTimeout(function(){
+				resolve(result)
+			},(Math.random()*1000 + 500))
+			})
+	}
+	//
+	$('.songLists').on('click','li',function(e){
+		$('.content>li').eq(0).removeClass('active')
+		$('.content>li').eq(1).addClass('active')
+	})
 })()
 
 
--- NetEase/js/song.js
+++ NetEase/js/song.js
@@ -20,7 +20,7 @@
 	  //获取数据，分割拼接成数值
 	  initText(song)
 	})
-
+	//音频的播放设置
   function initPlayer(url){
 	  $audio.attr('src', url)
 	  $audio[0].oncanplay = function(){
@@ -35,8 +35,35 @@
 			$audio[0].play()
 			$disc.addClass('playing')
 	  })
+	  //将歌曲时间转变成这种形式：00:14.01
+	  setInterval(()=>{
+		  let minute = ~~($audio[0].currentTime/60)
+	  	let second = $audio[0].currentTime - minute*60
+	  	if (minute<10) {
+	  		minute = '0'+ minute
+	  	}
+	  	if (second<10) {
+	  		second = '0'+ second
+	  	}
+	  	let time = `${minute}:${second}`
+	  	let $lines = $('.lyric>p')
+	  	//遍历对象，寻找time时间大于data-time的前一个且time时间小于data-time的后一个
+	  	for(var i = 0; i<$lines.length;i++){
+	  		if ($lines.eq(i).attr('data-time') < time && $lines.eq(i+1).attr('data-time') > time && $lines.eq(i+1) !== 0) {
+	  			$whichLine = ($lines.eq(i))
+	  		}
+	  	}
+	  	//css调整歌词高度变化
+	  	if ($whichLine) {
+	  		let lineTop = $whichLine.offset().top
+	  		let lyricTop = $('.lyric').offset().top
+	  		let delta = lineTop - lyricTop - $('.lyricShow').height()/3
+	  		$('.lyric').css('transform',`translateY(-${delta}px)`)
+	  		$whichLine.addClass('active').prev().removeClass('active')
+	  	}
+	  },500)
   }
-
+  //歌词的插入
   function initText(song){
 		$song.text(song.name+'-' +song.singer)
 		let lyric = song.lyric
@@ -58,6 +85,7 @@
 			$lyric.append($p)
 		})
   }
+
 })()
 
 
